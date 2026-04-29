84267369c6cb40c056896febca0cb5c8b20f0c53
THEGENTLEMAN31 <140864224+THEGENTLEMAN31@users.noreply.github.com>
2026-04-29 15:37:43 +0000

    Sentry integration and Monitoring logic

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from portal.views import home
+from portal.views import home , trigger_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), # La racine du site
+    path('', home, name='home'),
+    path('sentry-debug/', trigger_error, name='sentry_debug'),
 ]
